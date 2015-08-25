0d0bbd9457f338105a5bfcc8748ec893cf2d9ce3
Lukasz Baranowski <lukasz.baranowski@gmail.com>
2015-08-25 04:53:59 +0200

    Create deployable war

--- src/main/java/pl/lucek/pitstones/web/Main.java
+++ src/main/java/pl/lucek/pitstones/web/Main.java
@@ -2,10 +2,17 @@ package pl.lucek.pitstones.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main {
+public class Main extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Main.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
